08a86f5d29a63c967eb7844373a46238ca75bb5d
Miquel Carnot <mcgraphicss@gmail.com>
2024-07-04 17:43:09 +0200

    Changes - Erased route path /demo. - Added language in template.html - Removed unnecessary css. - Added star wars icon to navbar. - Changed navbar button for dropdown button. - card.jsx added. - Deleted readme.md of boilerplate.

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -1,10 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 
-export const Footer = () => (
-    <footer className="footer mt-auto py-3 text-center">
-        <p>
-            Made with <i className="fa fa-heart text-danger" /> by{" "}
-            <a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-        </p>
-    </footer>
-);
+export const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="container text-center text-white">
+                Made by{" "}
+                <a href="https://github.com/greyzeids" target="_blank" rel="">
+                    Miquel Carnot
+                </a>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -2,16 +2,50 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
-	);
+    return (
+        <nav className="navbar navbar-light bg-light">
+            <Link to="/" className="navbar-brand ms-3">
+                <img
+                    width="100"
+                    height="100"
+                    src="https://img.icons8.com/ios-filled/100/star-wars.png"
+                    alt="star-wars"
+                />
+            </Link>
+            <div className="ml-auto">
+                <div className="dropdown me-3">
+                    <button
+                        className="btn btn-primary dropdown-toggle"
+                        type="button"
+                        data-bs-toggle="dropdown"
+                        aria-expanded="false"
+                    >
+                        Favoritos
+                    </button>
+                    <ul className="dropdown-menu">
+                        <li>
+                            <Link to="/demo" className="dropdown-item">
+                                Action
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/another-demo" className="dropdown-item">
+                                Another action
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                to="/something-else"
+                                className="dropdown-item"
+                            >
+                                Something else here
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
 };
+
+export default Navbar;
